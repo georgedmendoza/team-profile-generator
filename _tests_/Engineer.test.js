1c267ebb8764eb5f5094180a9e6d4d46f26432ef
georgedmendoza <georgedmendoza13@gmail.com>
2021-08-21 19:04:18 -0500

    complete engineer test and js

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,9 +1,19 @@
-const Enginer = require('../lib/Engineer');
+const Engineer = require('../lib/Engineer');
 
 test('creates engineer object with github username', () => {
-    const engineer = new Enginer('George',13,'georgedmendoza13@gmail.com','georgedmendoza');
+    const engineer = new Engineer('George',13,'georgedmendoza13@gmail.com','georgedmendoza');
 
     expect(engineer.github).toEqual(expect.any(String));
 })
 
-test('')
+test('when clicked it opens new window for github username', () => {
+    const engineer = new Engineer('George',13,'georgedmendoza13@gmail.com','georgedmendoza');
+
+    expect(engineer.getGithub()).toEqual(expect.any(String));
+})
+
+test('overwrites role to output Engineer', () => {
+    const engineer = new Engineer('George',13,'georgedmendoza13@gmail.com','georgedmendoza');
+
+    expect(engineer.getRole()).toEqual(expect.stringContaining('Engineer'));
+})
